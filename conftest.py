3153217bf73b88f27745bb4b4e0aa4c4cb0151b7
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-15 00:12:40 +0900

    Updated not to use chrome_option

--- conftest.py
+++ conftest.py
@@ -1,17 +1,16 @@
 import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def google():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     if 'CHROME_DRIVER_PATH' in os.environ:
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'), chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'), options=chrome_options)
     else:
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://www.google.com")
     yield driver
     driver.quit()
