9472b4f7ffe9288656be7127cbd00f95b05f964d
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-14 23:12:27 +0900

    Updated to use fixture for initialize and finalize the driver

--- test_google.py
+++ test_google.py
@@ -1,11 +1,16 @@
 import os
+import pytest
 from selenium import webdriver
 
-def test_google():
+
+@pytest.fixture(scope='function', autouse=True)
+def google():
     driver = webdriver.Chrome(executable_path=os.environ['CHROME_DRIVER_PATH'])
     driver.get("http://www.google.com")
-
-    elm = driver.find_element_by_id('hplogo')
-    assert elm.get_attribute('alt') == 'Google'
-
+    yield driver
     driver.quit()
+
+
+def test_google(google):
+    elm = google.find_element_by_id('hplogo')
+    assert elm.get_attribute('alt') == 'Google'
