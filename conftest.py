b439cda80741a32dadb4123d2d7b77457f4d837e
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-14 23:41:05 +0900

    Updated a path to chromedriver

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def google():
-    path = os.getenv('CHROME_DRIVER_PATH', '')
+    path = os.getenv('CHROME_DRIVER_PATH', '/usr/local/bin/chromedriver')
     driver = webdriver.Chrome(executable_path=path)
     driver.get("http://www.google.com")
     yield driver
