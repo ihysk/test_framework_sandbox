49b3d8e7643cb462c1f0a29c9edb50cfebc79c20
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-14 23:57:25 +0900

    Using headless mode

--- conftest.py
+++ conftest.py
@@ -1,14 +1,17 @@
 import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def google():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
     if 'CHROME_DRIVER_PATH' in os.environ:
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'))
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'), chrome_options=chrome_options)
     else:
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("http://www.google.com")
     yield driver
     driver.quit()
