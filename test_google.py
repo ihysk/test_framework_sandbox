c0daf2b553b1fe6ecddde4971ecf7601a104e4ef
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-17 22:01:30 +0900

    Added new test case for checking input field

--- test_google.py
+++ test_google.py
@@ -1,3 +1,10 @@
-def test_google(google):
+def test_title_image(google):
     elm = google.find_element_by_id('hplogo')
     assert elm.get_attribute('alt') == 'Google'
+
+
+def test_input_field(google):
+    elm = google.find_element_by_name('q')
+    elm.clear()
+    elm.send_keys('hogehoge')
+    assert elm.get_attribute('value') == 'hogehoge'
