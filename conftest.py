3784d6cdf773009d5c02a304cbefae578c3d9dd8
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-17 22:00:46 +0900

    Using normal chrome for local testing (not headless)

--- conftest.py
+++ conftest.py
@@ -5,12 +5,12 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def google():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
     if 'CHROME_DRIVER_PATH' in os.environ:
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'), options=chrome_options)
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'))
     else:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
         driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://www.google.com")
     yield driver
