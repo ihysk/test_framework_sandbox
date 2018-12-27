8bc62af412dc918074380f443cc5e22b049c4667
ihysk <quest.for.curiosity1985@gmail.com>
2018-12-27 19:14:59 +0900

    Add code to take a screenshot when test fails

--- conftest.py
+++ conftest.py
@@ -3,8 +3,20 @@ import pytest
 from selenium import webdriver
 
 
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
 @pytest.fixture(scope='function', autouse=True)
-def google():
+def google(request):
     if 'CHROME_DRIVER_PATH' in os.environ:
         driver = webdriver.Chrome(executable_path=os.environ.get('CHROME_DRIVER_PATH'))
     else:
@@ -14,4 +26,11 @@ def google():
         driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://www.google.com")
     yield driver
+    if request.node.rep_call.failed:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(dir_path, 'screenshot-{}.png'.format(request.node.name))
+        try:
+            driver.save_screenshot(file_path)
+        except IOError as e:
+            print('I/O error({0}): {1}'.format(e.errno, e.strerror))
     driver.quit()
